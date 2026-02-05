2d7f234ad92a1518888c7038fe76af46c444a6f5
Madhu <Madhu@Madhu.lan>
2026-02-05 08:50:29 +0530

    my second commit

---
+++ src/program3.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class program3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("The string \"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a palindrome.");
+        }
+
+        sc.close();
+    }
+
+    // Method to check palindrome
+    public static boolean isPalindrome(String str) {
+        str = str.toLowerCase().replaceAll("\\s+", ""); // normalize: lowercase & remove spaces
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
